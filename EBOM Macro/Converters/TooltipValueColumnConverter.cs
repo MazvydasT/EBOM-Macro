0ac1f35b40e983dcc3e7b0de1e1c6ec0dd08d75f
Maz <mazvydas.tadaravicius@gmail.com>
2021-08-06 20:25:32 +0100

    Made tooltip Value column conditionally visible

---
+++ EBOM Macro/Converters/TooltipValueColumnConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EBOM_Macro.Converters
+{
+    public class TooltipValueColumnConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value == null ? Visibility.Collapsed : Visibility.Visible;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
