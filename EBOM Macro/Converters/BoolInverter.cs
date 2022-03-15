3ea37f15893277aac93dcee4f369dd09d40e7a7b
Maz T <mazvydas.tadaravicius@gmail.com>
2022-03-15 12:36:58 +0000

    Fixed translation system selection affecting other tabs

---
+++ EBOM Macro/Converters/BoolInverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EBOM_Macro.Converters
+{
+    public class BoolInverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => !(bool)value;
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => !(bool)value;
+    }
+}
