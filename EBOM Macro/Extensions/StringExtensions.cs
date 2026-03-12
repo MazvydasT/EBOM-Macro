7e843099b1da92a6e4c5e429c7beca6f3eea8d73
MazT <mazvydas.tadaravicius@gmail.com>
2026-03-12 13:30:36 +0000

    Exclude multi-byte characters from JT file name in FILENAME field

--- EBOM Macro/Extensions/StringExtensions.cs
+++ EBOM Macro/Extensions/StringExtensions.cs
@@ -1,9 +1,15 @@
 ﻿using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace EBOM_Macro.Extensions
 {
     public static class StringExtensions
     {
-        public static string GetSafeFileName(this string value) => string.Join("_", value.Split(Path.GetInvalidFileNameChars()));
+        static Encoding UTF8_ENCODING = Encoding.UTF8;
+        public static string GetSafeFileName(this string value) => string.Concat(
+            value.Split(Path.GetInvalidFileNameChars())
+                .SelectMany(s => s.Where(c => UTF8_ENCODING.GetBytes(c.ToString()).Length == 1))
+        );
     }
 }
